46d9d345dd2a673aa011b687011cf2f9de1a3bb5
Mikhail <mmmffjs7438g@gmail.com>
2020-02-11 22:25:54 +0200

    11.02.2020: 22:25

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import styles from "./App.module.css";
-import { getFriendsThunk, searchFriendThunk,getPhotosThunk } from "./redux/thunks/thunks";
+import {
+  getFriendsThunk,
+  searchFriendThunk,
+  getPhotosThunk
+} from "./redux/thunks/thunks";
 import { connect } from "react-redux";
 
 const Friend = props => {
@@ -11,9 +15,35 @@ const Friend = props => {
   );
 };
 const App = props => {
-  const { name_surname, getFriendsThunk, searchFriendThunk, name_surname_liked, getPhotosThunk } = props;
+  const {
+    name_surname,
+    getFriendsThunk,
+    searchFriendThunk,
+    name_surname_liked,
+    getPhotosThunk
+  } = props;
   let [valueTextInput, setValueTextInput] = useState("");
-  window.VK.Auth.login(null)
+
+  window.VK.init(
+    () => {
+      console.log("при успешной инициализации API");
+    },
+    () => {
+      console.log("ошибка инициализации API");
+    },
+    "5.103"
+  );
+
+
+  window.VK.api(
+    "execute",
+    {
+      code: "return {user: API.users.get({user_ids: 48437298,  v: 5.73}), friends: API.friends.get()};"
+    },
+    function(data) {
+      console.log(data);
+    }
+  );
   return (
     <div className={styles.App_wrapper}>
       <header className={styles.App_header}>VK API</header>
--- src/redux/thunks/thunks.js
+++ src/redux/thunks/thunks.js
@@ -2,9 +2,9 @@ import * as axios from "axios";
 import { areaFriend, nameSurnameLikedPeople } from "../actions/actions";
 // move to .env
 const api_token =
-  "714fbc730cfda583ff83e2ac47a5faf07ad1998cce1fcbe6cd01b44cdfbb09e3f338563a220af04ba19a4";
+  "7b0cb4e7a555154329829579c4f2098c17641ade88bf6ce391f0c4236db9f05efddf0713a8bb64124a003";
 const baseURL = "https://api.vk.com/method/";
-const user_id = 48437298;
+const user_id = 43463557;
 
 export const getFriendsThunk = () => dispatch =>
   axios
@@ -21,39 +21,47 @@ export const searchFriendThunk = search_line => dispatch =>
     )
     .then(response => dispatch(areaFriend(response.data.response.items)));
 
+
+
 export const getPhotosThunk = () => dispatch =>
-  axios
-    .get(
-      //получаем id фоток
-      `${baseURL}photos.getAll?owner_id=${user_id}&count=200&v=5.103&access_token=${api_token}`
-    )
-    .then(response => {
-      response.data.response.items.forEach(el =>
-        setTimeout(() => {
-          axios
-            .get(
-              //берем каждую id фотки и получаем массив id людей, которые лайкнули
-              `${baseURL}likes.getList?type=photo&owner_id=${user_id}&item_id=${el.id}&count=1000&v=5.103&access_token=${api_token}`
-            )
-            .then(response => {
-              let IdLikedPeople = "";
-              response.data.response.items.forEach(el => {
-                // в ответе объект, в котором массив id людей, которые лайкнули
-                IdLikedPeople === "" //для ускорения, а может и не только, берём все id из массива
-                  ? (IdLikedPeople = IdLikedPeople + el) //и кидаем их в строку через запятую, а потом делаем запрос
-                  : (IdLikedPeople = IdLikedPeople + "," + el); //в который кинем эту строку idшников и он мнесто них вернет массив имен и фамилий
-              });
-              axios
-                .get(
-                  `${baseURL}users.get?user_ids=${IdLikedPeople}&v=5.103&access_token=${api_token}`
-                )
-                .then(
-                  //массив имён и фамилий тех кто лайкнул каждую фотку
-                  response => {
-                    dispatch(nameSurnameLikedPeople(response.data.response));
-                  }
-                );
+  //получаем id фоток
+  window.VK.api(
+    "photos.getAll",
+    {
+      owner_id: user_id,
+      count: 200,
+      v: "5.103",
+      access_token: api_token
+    },
+    data => {  
+
+      data.response.items.forEach(el => {
+        window.VK.api(
+          "likes.getList", //берем каждую id фотки и получаем массив id людей, которые лайкнули
+          {
+            type: "photo",
+            owner_id: user_id,
+            item_id: el.id,
+            count: 1000,
+            v: 5.103,
+            access_token: api_token
+          },
+          data => { debugger;
+            let IdLikedPeople = "";
+            data.data.response.items.forEach(el => {
+              // в ответе объект, в котором массив id людей, которые лайкнули
+              IdLikedPeople === "" //для ускорения, а может и не только, берём все id из массива
+                ? (IdLikedPeople = IdLikedPeople + el) //и кидаем их в строку через запятую, а потом делаем запрос
+                : (IdLikedPeople = IdLikedPeople + "," + el); //в который кинем эту строку idшников и он мнесто них вернет массив имен и фамилий
             });
-        }, 30000)
-      );
-    });
+            window.VK.api(
+              "users.get",
+              { user_ids: IdLikedPeople, v: "5.103", access_token: api_token },
+              response =>
+                dispatch(nameSurnameLikedPeople(response.data.response))
+            );
+          }
+        );
+      });
+    }
+  );
